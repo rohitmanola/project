ced796a4cf9f621de2ccf225d166716a1af56846
Rohit Manola <manolarohit24@gmail.com>
2024-01-30 15:08:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 
-
 export default function App() {
   const [countries, setCountries] = useState([]);
 
@@ -10,6 +9,7 @@ export default function App() {
       .then((data) => setCountries(data))
       .catch((err) => console.error("Error fetching data: ", err));
   }, []);
+
   const cardStyle = {
     width: "200px",
     border: "1px solid #ccc",
@@ -39,7 +39,7 @@ export default function App() {
         <div key={country.cca3} style={cardStyle}>
           <img
             src={country.flags.png}
-            alt={"Flag of ${country.name.common} "}
+            alt={`Flag of ${country.name.common}`}
             style={imageStyle}
           />
           <h2>{country.name.common}</h2>
