be7269a34235f039d14fde4695844294efcfbd7b
rohitmanola <69840411+rohitmanola@users.noreply.github.com>
2024-02-01 06:24:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import { useEffect, useState } from "react";
-import "./styles.css";
+import "./App.css";
 
-export default function App() {
+function App() {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => setCountries(data))
-      .catch((err) => console.error("Error fetching data: ", err));
+      .catch((error) => console.error("Error fetching data: ", error));
   }, []);
+
   const cardStyle = {
     width: "200px",
     border: "1px solid #ccc",
     borderRadius: "10px",
+    margin: "10px",
     padding: "10px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-  };
-
-  const imageStyle = {
-    width: "100px",
-    height: "100px",
+    justifyContent: "center",
   };
 
   const containerStyle = {
@@ -33,18 +31,21 @@ export default function App() {
     height: "100vh",
   };
 
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
+
   return (
     <div style={containerStyle}>
       {countries.map((country) => (
         <div key={country.cca3} style={cardStyle}>
-          <img
-            src={country.flags.png}
-            alt={`Flag of ${country.name.common} `}
-            style={imageStyle}
-          />
+          <img src={country.flags.png} alt={`Flag of ${country.name.common}`} style={imageStyle} />
           <h2>{country.name.common}</h2>
         </div>
       ))}
     </div>
   );
 }
+
+export default App;
